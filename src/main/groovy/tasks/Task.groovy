cc0306abf7348d68b5ba84e6dbb142ea60cce48f
nichita <nichita@cadvegra.com>
2018-03-10 13:32:03 +0000

    added tasks

---
+++ src/main/groovy/tasks/Task.groovy
@@ -0,0 +1,10 @@
+package tasks
+
+class Task {
+
+    int daysLeftUntilDeadline
+    int workHoursLeft
+
+    Closure<BigDecimal> averageWorkHoursPerDay = { workHoursLeft/daysLeftUntilDeadline }
+
+}
--- src/main/java/nagg/NaggApp.java
+++ src/main/java/nagg/NaggApp.java
@@ -1,17 +1,16 @@
 package nagg;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.Calendar.Style;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.view.CalendarView;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class NaggApp extends Application {
 
@@ -20,50 +19,50 @@ public class NaggApp extends Application {
 
             CalendarView calendarView = new CalendarView();
 
-                Calendar birthdays = new Calendar("Birthdays");
-                Calendar holidays = new Calendar("Holidays");
+            Calendar birthdays = new Calendar("Birthdays");
+            Calendar holidays = new Calendar("Holidays");
 
-                birthdays.setStyle(Style.STYLE1);
-                holidays.setStyle(Style.STYLE2);
+            birthdays.setStyle(Style.STYLE1);
+            holidays.setStyle(Style.STYLE2);
 
-                CalendarSource myCalendarSource = new CalendarSource("My Calendars");
-                myCalendarSource.getCalendars().addAll(birthdays, holidays);
+            CalendarSource myCalendarSource = new CalendarSource("My Calendars");
+            myCalendarSource.getCalendars().addAll(birthdays, holidays);
 
-                calendarView.getCalendarSources().addAll(myCalendarSource);
+            calendarView.getCalendarSources().addAll(myCalendarSource);
 
-                calendarView.setRequestedTime(LocalTime.now());
+            calendarView.setRequestedTime(LocalTime.now());
 
-                Thread updateTimeThread = new Thread("Calendar: Update Time Thread") {
-                        @Override
-                        public void run() {
-                                while (true) {
-                                        Platform.runLater(() -> {
-                                                calendarView.setToday(LocalDate.now());
-                                                calendarView.setTime(LocalTime.now());
-                                        });
+            Thread updateTimeThread = new Thread("Calendar: Update Time Thread") {
+                @Override
+                public void run() {
+                    while (true) {
+                        Platform.runLater(() -> {
+                            calendarView.setToday(LocalDate.now());
+                            calendarView.setTime(LocalTime.now());
+                        });
 
-                                        try {
-                                                // update every 10 seconds
-                                                sleep(10000);
-                                        } catch (InterruptedException e) {
-                                                e.printStackTrace();
-                                        }
+                        try {
+                            // update every 10 seconds
+                            sleep(10000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
 
-                                }
-                        };
-                };
+                    }
+                }
+            };
 
-                updateTimeThread.setPriority(Thread.MIN_PRIORITY);
-                updateTimeThread.setDaemon(true);
-                updateTimeThread.start();
+            updateTimeThread.setPriority(Thread.MIN_PRIORITY);
+            updateTimeThread.setDaemon(true);
+            updateTimeThread.start();
 
-                Scene scene = new Scene(calendarView);
-                primaryStage.setTitle("Calendar");
-                primaryStage.setScene(scene);
-                primaryStage.setWidth(1300);
-                primaryStage.setHeight(1000);
-                primaryStage.centerOnScreen();
-                primaryStage.show();
+            Scene scene = new Scene(calendarView);
+            primaryStage.setTitle("Calendar");
+            primaryStage.setScene(scene);
+            primaryStage.setWidth(1300);
+            primaryStage.setHeight(1000);
+            primaryStage.centerOnScreen();
+            primaryStage.show();
         }
 
         public static void main(String[] args) {
