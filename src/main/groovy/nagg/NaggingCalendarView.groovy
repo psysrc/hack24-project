d191f5ff31b85a7b89ef159a4a9515943c92a1cd
Sam <sam.copping@yahoo.com>
2018-03-11 01:15:23 +0000

    Added a dialog box to get input data
    The box gets the new task title and how many hours the task should be worked.

--- src/main/groovy/nagg/NaggingCalendarView.groovy
+++ src/main/groovy/nagg/NaggingCalendarView.groovy
@@ -5,8 +5,18 @@ import com.calendarfx.model.Interval
 import com.calendarfx.view.CalendarView
 import com.calendarfx.view.DateControl
 import com.calendarfx.view.VirtualGrid
+import impl.com.calendarfx.view.NumericTextField
+import javafx.application.Platform
+import javafx.geometry.Insets
 import javafx.scene.control.Alert
+import javafx.scene.control.ButtonBar
+import javafx.scene.control.ButtonType
+import javafx.scene.control.Dialog
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
 import javafx.scene.control.TextInputDialog
+import javafx.scene.layout.GridPane
+import javafx.util.Pair
 
 import java.text.MessageFormat
 import java.time.DayOfWeek
@@ -23,6 +33,55 @@ class NaggingCalendarView extends CalendarView{
 
             // TODO: Add a dialog box here to get information about the new task to be added
             // vbox, hbox, label, text field, button
+
+            // Create the dialog box
+            Dialog<Pair<String, Integer>> dialog = new Dialog<>();
+            dialog.setTitle("Task Input");
+
+            // Add the buttons
+            ButtonType ok = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+            dialog.getDialogPane().getButtonTypes().addAll(ok, ButtonType.CANCEL);
+
+            // Set up a new gridpane
+            GridPane gridPane = new GridPane();
+            gridPane.setHgap(10);
+            gridPane.setVgap(10);
+            gridPane.setPadding(new Insets(20, 150, 10, 10));
+
+            // Set up new text fields
+            TextField name = new TextField();
+            name.setPromptText("Title");
+            NumericTextField hours = new NumericTextField();
+            hours.setPromptText("Hours of Work");
+
+            // Add text fields and labels to gridpane
+            // TODO: Add new label and fix positions of everything
+            gridPane.add(name, 0, 0);
+            gridPane.add(new Label("To:"), 1, 0);
+            gridPane.add(hours, 2, 0);
+
+            // Set the dialog box contents to that of the gridpane
+            dialog.getDialogPane().setContent(gridPane);
+
+            // Request focus on the name field by default.
+            Platform.runLater{ name.requestFocus()}
+
+            // Convert the result to a name-hours-pair when the OK button is clicked.
+            dialog.setResultConverter {dialogButton ->
+                if (dialogButton == ok) {
+                    return new Pair<>(name.getText(), hours.getText());
+                }
+                return null;
+            };
+
+            // Show the dialog box
+            Optional<Pair<String, String>> result = dialog.showAndWait();
+
+            // If we return a good result, do stuff
+            result.ifPresent{pair ->
+                System.out.println("Name=" + pair.getKey() + ", Hours=" + pair.getValue());
+            };
+
 
             DateControl control = param.getDateControl();
 
@@ -43,8 +102,6 @@ class NaggingCalendarView extends CalendarView{
             Interval interval = new Interval(time.toLocalDateTime(), time.toLocalDateTime().plusHours(1));
             deadline.setInterval(interval);
 
-            TextInputDialog tld = new TextInputDialog("Thing");
-            tld.showAndWait()
 
 
 //            if (control instanceof AllDayView) {
@@ -62,8 +119,6 @@ class NaggingCalendarView extends CalendarView{
 //            }
 //
 //            events.add(entry2)
-
-            new Alert(Alert.AlertType.INFORMATION).showAndWait()
 
 //            println events
             addLastEntry()
