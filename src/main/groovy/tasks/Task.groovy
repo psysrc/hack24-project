9db693a4467806ff58b9fdc4ba09ac438d37b964
nichita <nichita@cadvegra.com>
2018-03-11 00:22:02 +0000

    added some task functionality

--- src/main/groovy/tasks/Task.groovy
+++ src/main/groovy/tasks/Task.groovy
@@ -1,17 +1,45 @@
 package tasks
 
 import com.calendarfx.model.Entry
+import com.calendarfx.model.Interval
+import nagg.Deadline
+
+import java.text.MessageFormat
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 class Task {
 
     String name
-    int daysLeftUntilDeadline
+    Deadline endDate
+    int totalHours
     int workHoursLeft
 
-    Closure<BigDecimal> averageWorkHoursPerDay = { workHoursLeft/daysLeftUntilDeadline }
+    final Closure<Long> daysUntilEnd = { ChronoUnit.DAYS.between(LocalDateTime.now(), endDate.getStartDate()) }
+    final Closure<Integer> averageWorkHoursPerDay = { (int)Math.ceil(workHoursLeft / daysUntilEnd.call() as double) }
+
+    Task(String name, Deadline endDate, int hours) {
+        this.name = name
+        this.endDate = endDate
+        this.workHoursLeft = totalHours = hours
+    }
 
     List<Entry> genEntries(){
-        // cool stuff happens here
+        int noEntries = averageWorkHoursPerDay.call()
+        List<Entry> entries = new ArrayList<>()
+
+        noEntries.times {
+            Entry<Object> en = new Entry<>(MessageFormat.format("Work on {0}", name)); //$NON-NLS-1$
+            def when = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS).plusDays(it)
+            Interval interval2 = new Interval(when, when.plusHours(20))
+
+            en.setFullDay(true)
+
+            entries.add(en)
+
+        }
+
+        return entries
 
     }
 
